d7a3e0ee2a10f6f5c4713738a933be488b5984f3
Matt Rose <mattrose@folkwolf.net>
2011-04-28 18:36:08 -0400

    fixing random stuff

--- twitcmd.rb
+++ twitcmd.rb
@@ -1,29 +1,28 @@
 require "rubygems"
 require "twitter"
 require 'highline/import'
-require 'getoptlong'
 
 post = nil
 count = 200
-opts = GetoptLong.new( ["--post","-p", GetoptLong::REQUIRED_ARGUMENT],
-                       ["--count","-c", GetoptLong::REQUIRED_ARGUMENT])
-opts.each {|o,a|
-  case o
-    when "--post"
-      post = a
-    when "--count"
-      count = a.to_i
-  end
-}
-puts post
-puts count
 
-
+if ARGV[0] == "post"
+	post = ARGV[1..-1]
+end
 since_id = nil
 cf = "#{ENV['HOME']}/.twitcmd"
 cfs = "#{ENV['HOME']}/.twitcmd_since"
 if File.exists?(cf)
 	eval(open(cf).read)
+	if CONSUMER_KEY && CONSUMER_SECRET
+		puts "consumer key read"
+	else
+		CONSUMER_KEY = "f8ZLLDDCHXfq7zw04JwpDA"
+		CONSUMER_SECRET = "GPe5jNFPJC6vahuYnZ7tei1LbuPJwv9RljBnsAIEEs"
+	end
+	
+else
+	puts "no cf file"
+	exit
 end
 if File.exists?(cfs)
         since_id = open(cfs).read.to_i
@@ -32,10 +31,10 @@ end
 # Certain methods require authentication. To get your Twitter OAuth credentials,
 # register an app at http://dev.twitter.com/apps
 Twitter.configure do |config|
-  config.consumer_key = YOUR_CONSUMER_KEY
-  config.consumer_secret = YOUR_CONSUMER_SECRET
-  config.oauth_token = YOUR_OAUTH_TOKEN
-  config.oauth_token_secret = YOUR_OAUTH_TOKEN_SECRET
+  config.consumer_key = CONSUMER_KEY
+  config.consumer_secret = CONSUMER_SECRET
+  config.oauth_token = OAUTH_TOKEN
+  config.oauth_token_secret = OAUTH_TOKEN_SECRET
 end
 
 # Initialize your Twitter client
@@ -66,13 +65,38 @@ def get_reply_chain(status)
 end
 
 
-# Read the most recent status update in your home timeline
-#client.home_timeline.first.methods.each { |a| puts a }
 new_since = client.home_timeline.first.id
 args = { :include_entities => 't' } 
 args = { :include_entities => 't', :count => 200, :since_id => since_id } if since_id
+msgs = []
+#msgs = client.home_timeline(args)
+max_id = nil
+last_max_id = 0
+while max_id != last_max_id do 
+	if max_id
+		tmsgs = client.home_timeline(:count => 200, :since_id => since_id, :max_id => max_id)
+		last_max_id = msgs.last.id
+	else 
+		tmsgs = client.home_timeline(:count => 200, :since_id => since_id)
+		last_max_id = 0
+	end
+	max_id = tmsgs.last.id
+	msgs.concat(tmsgs)
+end
+if msgs == []
+        puts "no new messages"
+        exit
+end
+
+#puts "no more messages"
+msgs.delete_at(-1)
+num = msgs.length
 puts "<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>"
-client.home_timeline(args).reverse.each { |a| 
+puts "#{msgs.length} new message since last time this was run"
+puts "the last message you read was from #{Twitter.status(since_id.to_i).created_at}"
+
+msgs.reverse.each_with_index { |a,i| 
+	puts "message #{i + 1} of #{num}"
 	puts a.user.name 
 	puts a.text 
 	puts "http://twitter.com/#!/#{a.user.screen_name}/status/#{a.id}" 
@@ -88,8 +112,8 @@ client.home_timeline(args).reverse.each { |a|
 	puts "---------------------------------------------------------------"
 	cmd = ask("reply, retweet, next or quit:  ", ["","r","rt","n","q"]) { |q| q.readline }
 	if cmd == "q"
-		break
 		new_since = a.id
+		break
 	elsif cmd == "n" or cmd == ""
 		next
 	end
